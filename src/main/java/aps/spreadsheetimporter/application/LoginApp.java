11c109a4eba1a8cc86132694f04fae5633e4e9a9
André P. Silva <4ndpsilva@gmail.com>
2021-04-11 14:56:32 -0300

    Implementações de funcionalidades básicas de CRUD (#1)

---
+++ src/main/java/aps/spreadsheetimporter/application/LoginApp.java
@@ -0,0 +1,25 @@
+package aps.spreadsheetimporter.application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginApp {
+    private Stage stage;
+
+    public LoginApp(final Stage stage){
+        this.stage = stage;
+    }
+
+    public void show() throws Exception{
+        final Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+        root.setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100%, blue 0%, silver 100%);");
+        final Scene scene = new Scene(root, 450, 250);
+        stage.setScene(scene);
+        stage.setTitle("Login");
+        stage.setMaximized(false);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
---
+++ src/main/java/aps/spreadsheetimporter/controller/PersonEditController.java
@@ -0,0 +1,95 @@
+package aps.spreadsheetimporter.controller;
+
+import aps.spreadsheetimporter.model.Person;
+import aps.spreadsheetimporter.util.DateUtil;
+import aps.spreadsheetimporter.util.MessageUtil;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import lombok.Getter;
+import lombok.Setter;
+
+public class PersonEditController {
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private DatePicker txtBirthday;
+
+    @Setter
+    private Stage dialogStage;
+
+    private Person person;
+
+    @Getter
+    private boolean okClicked = false;
+
+    @FXML
+    private void initialize(){
+        txtName.requestFocus();
+        txtName.selectAll();
+    }
+
+    @FXML
+    private void handleSave(){
+        if(isValid()){
+            person.name(txtName.getText());
+            person.lastName(txtLastName.getText());
+            person.birthday(DateUtil.parse(txtBirthday.getEditor().getText()));
+
+            okClicked = true;
+            dialogStage.close();
+        }
+    }
+
+    @FXML
+    private void handleCancel(){
+        this.dialogStage.close();
+    }
+
+    private boolean isValid() {
+        final StringBuilder sbErrors = new StringBuilder();
+
+        if(txtName.getText() == null || txtName.getText().isEmpty()){
+            sbErrors.append("Informe o Nome\n");
+        }
+
+        if(txtLastName.getText() == null || txtLastName.getText().isEmpty()){
+            sbErrors.append("Informe o Sobrenome\n");
+        }
+
+        final String dataNascimento = txtBirthday.getEditor().getText();
+
+        if(dataNascimento == null || dataNascimento.isEmpty()){
+            sbErrors.append("Informe a Data de Nascimento\n");
+        }
+        else{
+            if(!DateUtil.validDate(dataNascimento)){
+                sbErrors.append("Data de Nascimento inválida\n");
+            }
+        }
+
+        if(sbErrors.length() > 0){
+            MessageUtil.showError("Por favor, corrija as informações a seguir", sbErrors.toString());
+            return false;
+        }
+
+        return true;
+    }
+
+    public void setPerson(final Person person){
+        this.person = person;
+        loadPerson(person);
+    }
+
+    private void loadPerson(final Person person){
+        final boolean isNotNull = person != null;
+        txtName.setText(isNotNull ? person.name() : "");
+        txtLastName.setText(isNotNull ? person.lastName() : "");
+        txtBirthday.getEditor().setText(isNotNull ? DateUtil.format(person.birthday()) : "");
+    }
+}
